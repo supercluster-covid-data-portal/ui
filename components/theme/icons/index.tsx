8f8db06f722dbfc24e7437be7b23cfaaf333e686
Ann Catton <ann.catton@oicr.on.ca>
2020-09-28 11:11:41 -0400

    Update icon imports

--- components/theme/icons/index.tsx
+++ components/theme/icons/index.tsx
@@ -4,5 +4,20 @@ import GitHubLogo from './github';
 import LinkedInLogo from './linkedin';
 import OrcidLogo from './orcid';
 import Illustration from './illustration';
+import Avatar from './avatar';
+import ChevronDown from './chevron_down';
+import OvertureLogo from './overture_logo';
+import OvertureLogoWithText from './overture_logo_with_text';
 
-export { GoogleLogo, FacebookLogo, GitHubLogo, LinkedInLogo, OrcidLogo, Illustration };
+export {
+  GoogleLogo,
+  FacebookLogo,
+  GitHubLogo,
+  LinkedInLogo,
+  OrcidLogo,
+  Illustration,
+  Avatar,
+  ChevronDown,
+  OvertureLogo,
+  OvertureLogoWithText,
+};
