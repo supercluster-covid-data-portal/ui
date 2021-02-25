eb89ed96f08fcf0602f4ef0eacc924936870f40d
Ann Catton <ann.catton@oicr.on.ca>
2021-02-25 16:28:24 -0500

    🚸 Add config error handling for repository

--- components/pages/repository/index.tsx
+++ components/pages/repository/index.tsx
@@ -7,6 +7,7 @@ import { RepoFiltersType } from './sqonTypes';
 import { getConfig } from '../../../global/config';
 import { css } from '@emotion/core';
 import createArrangerFetcher from '../../utils/arrangerFetcher';
+import { useEffect, useState } from 'react';
 
 const Arranger = dynamic(
   () => import('@arranger/components/dist/Arranger').then((comp) => comp.Arranger),
@@ -36,6 +37,126 @@ export interface PageContentProps {
 
 const arrangerFetcher = createArrangerFetcher({});
 
+const projectsQuery = `
+  query {
+    projects {
+      id
+      active
+      indices {
+        id
+        esIndex
+        graphqlField
+      }
+    }
+  }
+`;
+
+type Project = {
+  id: string;
+  active: boolean;
+  indices: [{ id: string; esIndex: string; graphqlField: string }];
+};
+
+const ConfigErrorDisplay = ({ children }: { children: React.ReactNode }) => (
+  <div
+    css={css`
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    `}
+  >
+    <div
+      css={(theme) =>
+        css`
+          padding: 2rem;
+          border: 1px solid ${theme.colors.grey_3};
+          border-radius: 5px;
+          ${theme.shadow.default};
+          ${theme.typography.heading};
+          max-width: 600px;
+        `
+      }
+    >
+      {children}
+    </div>
+  </div>
+);
+
+const getConfigError = ({
+  availableProjects,
+  projectId,
+  index,
+  graphqlField,
+}: {
+  availableProjects: Project[];
+  projectId: string;
+  index: string;
+  graphqlField: string;
+}) => {
+  const Missing = (
+    <span
+      css={(theme) =>
+        css`
+          color: ${theme.colors.error_dark};
+        `
+      }
+    >
+      Missing
+    </span>
+  );
+  if (!(projectId && index && graphqlField)) {
+    return (
+      <span>
+        One or more of project ID, Elasticsearch index, and index alias (graphQL field) values
+        required by the DMS portal do not exist. Make sure the values are specified in the
+        config.yaml file during DMS installation and have been used to create your project in the
+        Arranger Admin UI.
+        <ul>
+          <li>Project ID: {projectId || Missing}</li>
+          <li>Alias name: {graphqlField || Missing}</li>
+          <li>Elasticsearch index: {index || Missing}</li>
+        </ul>
+      </span>
+    );
+  }
+  if (!availableProjects.filter((project) => project.active).length) {
+    return (
+      <span>
+        No active projects for the DMS portal exist. Make sure the project specified in the
+        config.yaml file during DMS installation has been created in the Arranger Admin UI.
+      </span>
+    );
+  }
+  const foundProject =
+    availableProjects.length && availableProjects.find((project) => project.id === projectId);
+  if (!foundProject) {
+    return (
+      <span>
+        The project ID "{projectId}" configured for the DMS portal does not match any existing
+        project. Make sure the project ID specified in the config.yaml file during DMS installation
+        has been created in the Arranger Admin UI.
+      </span>
+    );
+  }
+
+  const aliasFromList = foundProject.indices.find((i) => i.id.match(projectId))?.graphqlField;
+  const matchesConfiguredAlias = graphqlField === aliasFromList;
+
+  if (!matchesConfiguredAlias) {
+    return (
+      <div>
+        The Elasticsearch alias name (graphQL field) "{aliasFromList}" required by the DMS portal
+        for project ID "{projectId}" does not match your configured alias name "{graphqlField}" .
+        Make sure the value specified in the config.yaml file during DMS installation has been used
+        to create your project in the Arranger Admin UI.
+      </div>
+    );
+  }
+  return null;
+};
+
 const RepositoryPage = () => {
   const {
     NEXT_PUBLIC_ARRANGER_PROJECT_ID,
@@ -43,38 +164,41 @@ const RepositoryPage = () => {
     NEXT_PUBLIC_ARRANGER_INDEX,
   } = getConfig();
 
+  const [availableProjects, setAvailableProjects] = useState<Project[]>([]);
+  useEffect(() => {
+    fetch(`https://arranger.qa.overture.bio/admin/graphql`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        variables: {},
+        query: projectsQuery,
+      }),
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('Could not retrieve available projects from Arranger server!');
+        }
+        return res.json();
+      })
+      .then(({ data: { projects } }: { data: { projects: Project[] } }) => {
+        setAvailableProjects(projects);
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  }, []);
+
+  const ConfigError = getConfigError({
+    availableProjects,
+    projectId: NEXT_PUBLIC_ARRANGER_PROJECT_ID,
+    index: NEXT_PUBLIC_ARRANGER_INDEX,
+    graphqlField: NEXT_PUBLIC_ARRANGER_GRAPHQL_FIELD,
+  });
+
   return (
     <PageLayout>
-      {/* TODO: arranger config error handling tbd */}
-      {!(
-        NEXT_PUBLIC_ARRANGER_PROJECT_ID &&
-        NEXT_PUBLIC_ARRANGER_GRAPHQL_FIELD &&
-        NEXT_PUBLIC_ARRANGER_INDEX
-      ) ? (
-        <div
-          css={css`
-            display: flex;
-            flex: 1;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          `}
-        >
-          <div
-            css={(theme) =>
-              css`
-                ${theme.typography.subheading}
-              `
-            }
-          >
-            Arranger is missing configuration values. Please check your ".env" file.
-            <ul>
-              <li>Project ID: {NEXT_PUBLIC_ARRANGER_PROJECT_ID || 'missing'}</li>
-              <li>GraphQL Field: {NEXT_PUBLIC_ARRANGER_GRAPHQL_FIELD || 'missing'}</li>
-              <li>Index: {NEXT_PUBLIC_ARRANGER_INDEX || 'missing'}</li>
-            </ul>
-          </div>
-        </div>
+      {ConfigError ? (
+        <ConfigErrorDisplay>{ConfigError}</ConfigErrorDisplay>
       ) : (
         <Arranger
           api={arrangerFetcher}
