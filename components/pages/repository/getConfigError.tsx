8a2c2c6084c588e5cb159bb9a701ee30b8f5417a
Ann Catton <ann.catton@oicr.on.ca>
2021-02-26 15:18:55 -0500

    💄 Styling for error messages, cleanup

---
+++ components/pages/repository/getConfigError.tsx
@@ -0,0 +1,211 @@
+import { css } from '@emotion/core';
+
+import { Checkmark, Warning } from '../../theme/icons';
+import theme from '../../theme/';
+import StyledLink from '../../Link';
+
+import { getConfig } from '../../../global/config';
+import { Project } from './';
+
+const ArrangerAdminUILink = () => {
+  const { NEXT_PUBLIC_ARRANGER_API } = getConfig();
+  const splitApi: string[] = NEXT_PUBLIC_ARRANGER_API.split('//');
+  const adminUiUrl: string = [splitApi[0], '//ui.', splitApi[1]].join('');
+  return (
+    <StyledLink href={adminUiUrl} target="_blank">
+      Arranger Admin UI
+    </StyledLink>
+  );
+};
+
+const WarningListItem = ({ fieldName }: { fieldName: string }) => (
+  <li
+    css={(theme) =>
+      css`
+        color: ${theme.colors.error_dark};
+        display: flex;
+        align-items: center;
+      `
+    }
+  >
+    <Warning height={16} width={16} />
+    <span
+      css={css`
+        padding-left: 6px;
+      `}
+    >
+      {fieldName}:{' '}
+      <span
+        css={css`
+          font-weight: bold;
+        `}
+      >
+        Missing
+      </span>
+    </span>
+  </li>
+);
+
+const ListItem = ({ fieldName, value }: { fieldName: string; value: string }) => (
+  <li
+    css={css`
+      display: flex;
+      align-items: center;
+    `}
+  >
+    <Checkmark height={16} width={16} fill={theme.colors.primary} />
+    <span
+      css={css`
+        padding-left: 6px;
+      `}
+    >
+      {fieldName}:{' '}
+      <span
+        css={css`
+          font-weight: bold;
+        `}
+      >
+        {value}
+      </span>
+    </span>
+  </li>
+);
+
+const getConfigError = ({
+  availableProjects,
+  projectId,
+  index,
+  graphqlField,
+}: {
+  availableProjects: Project[];
+  projectId: string;
+  index: string;
+  graphqlField: string;
+}) => {
+  if (!(projectId && index && graphqlField)) {
+    return (
+      <span>
+        One or more of the following values required by the DMS portal do not exist. Please make
+        sure the values are specified in the DMS{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          config.yaml
+        </span>{' '}
+        file during installation and have been used to create your project in the{' '}
+        <ArrangerAdminUILink />.
+        <ul
+          css={css`
+            list-style-type: none;
+            padding-left: 0px;
+          `}
+        >
+          {[
+            { field: 'Project ID', value: projectId },
+            { field: 'Alias name', value: graphqlField },
+            { field: 'Elasticsearch index', value: index },
+          ].map(({ field, value }) => {
+            return value ? (
+              <ListItem key={`${field}-${value}`} fieldName={field} value={value} />
+            ) : (
+              <WarningListItem key={`${field}-${value}`} fieldName={field} />
+            );
+          })}
+        </ul>
+      </span>
+    );
+  }
+
+  if (!availableProjects.filter((project) => project.active).length) {
+    return (
+      <span>
+        No active projects for the DMS portal exist. Please make sure the project specified in the
+        DMS{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          config.yaml
+        </span>{' '}
+        file during installation has been created in the <ArrangerAdminUILink />.
+      </span>
+    );
+  }
+
+  const foundProject =
+    availableProjects.length && availableProjects.find((project) => project.id === projectId);
+
+  if (!foundProject) {
+    return (
+      <span>
+        The project ID:{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          "{projectId}"
+        </span>{' '}
+        configured for the DMS portal does not match any existing project. Please make sure the
+        project ID specified in the DMS{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          config.yaml
+        </span>{' '}
+        file during installation has been created in the <ArrangerAdminUILink />.
+      </span>
+    );
+  }
+
+  const aliasFromList = foundProject.indices.find((i) => i.id.match(projectId))?.graphqlField;
+  const matchesConfiguredAlias = graphqlField === aliasFromList;
+
+  if (!matchesConfiguredAlias) {
+    return (
+      <div>
+        The Elasticsearch alias name (graphQL field):{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          "{aliasFromList}"
+        </span>{' '}
+        required by the DMS portal for project ID:{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          "{projectId}"
+        </span>{' '}
+        does not match your configured alias name:{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          "{graphqlField}"
+        </span>
+        . Please make sure the value specified in the DMS{' '}
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          config.yaml
+        </span>{' '}
+        file during installation has been used to create your project in the <ArrangerAdminUILink />
+        .
+      </div>
+    );
+  }
+};
+
+export default getConfigError;
--- components/pages/repository/index.tsx
+++ components/pages/repository/index.tsx
@@ -5,9 +5,10 @@ import PageLayout from '../../PageLayout';
 
 import { RepoFiltersType } from './sqonTypes';
 import { getConfig } from '../../../global/config';
-import { css } from '@emotion/core';
 import createArrangerFetcher from '../../utils/arrangerFetcher';
 import { useEffect, useState } from 'react';
+import ErrorContainer from '../../ErrorContainer';
+import getConfigError from './getConfigError';
 
 const Arranger = dynamic(
   () => import('@arranger/components/dist/Arranger').then((comp) => comp.Arranger),
@@ -35,6 +36,12 @@ export interface PageContentProps {
   fetchData?: (projectId: string) => Promise<any>;
 }
 
+export type Project = {
+  id: string;
+  active: boolean;
+  indices: [{ id: string; esIndex: string; graphqlField: string }];
+};
+
 const arrangerFetcher = createArrangerFetcher({});
 
 const projectsQuery = `
@@ -50,112 +57,6 @@ const projectsQuery = `
     }
   }
 `;
-
-type Project = {
-  id: string;
-  active: boolean;
-  indices: [{ id: string; esIndex: string; graphqlField: string }];
-};
-
-const ConfigErrorDisplay = ({ children }: { children: React.ReactNode }) => (
-  <div
-    css={css`
-      display: flex;
-      flex: 1;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    `}
-  >
-    <div
-      css={(theme) =>
-        css`
-          padding: 2rem;
-          border: 1px solid ${theme.colors.grey_3};
-          border-radius: 5px;
-          ${theme.shadow.default};
-          ${theme.typography.heading};
-          max-width: 600px;
-        `
-      }
-    >
-      {children}
-    </div>
-  </div>
-);
-
-const getConfigError = ({
-  availableProjects,
-  projectId,
-  index,
-  graphqlField,
-}: {
-  availableProjects: Project[];
-  projectId: string;
-  index: string;
-  graphqlField: string;
-}) => {
-  const Missing = (
-    <span
-      css={(theme) =>
-        css`
-          color: ${theme.colors.error_dark};
-        `
-      }
-    >
-      Missing
-    </span>
-  );
-  if (!(projectId && index && graphqlField)) {
-    return (
-      <span>
-        One or more of project ID, Elasticsearch index, and index alias (graphQL field) values
-        required by the DMS portal do not exist. Make sure the values are specified in the
-        config.yaml file during DMS installation and have been used to create your project in the
-        Arranger Admin UI.
-        <ul>
-          <li>Project ID: {projectId || Missing}</li>
-          <li>Alias name: {graphqlField || Missing}</li>
-          <li>Elasticsearch index: {index || Missing}</li>
-        </ul>
-      </span>
-    );
-  }
-  if (!availableProjects.filter((project) => project.active).length) {
-    return (
-      <span>
-        No active projects for the DMS portal exist. Make sure the project specified in the
-        config.yaml file during DMS installation has been created in the Arranger Admin UI.
-      </span>
-    );
-  }
-  const foundProject =
-    availableProjects.length && availableProjects.find((project) => project.id === projectId);
-  if (!foundProject) {
-    return (
-      <span>
-        The project ID "{projectId}" configured for the DMS portal does not match any existing
-        project. Make sure the project ID specified in the config.yaml file during DMS installation
-        has been created in the Arranger Admin UI.
-      </span>
-    );
-  }
-
-  const aliasFromList = foundProject.indices.find((i) => i.id.match(projectId))?.graphqlField;
-  const matchesConfiguredAlias = graphqlField === aliasFromList;
-
-  if (!matchesConfiguredAlias) {
-    return (
-      <div>
-        The Elasticsearch alias name (graphQL field) "{aliasFromList}" required by the DMS portal
-        for project ID "{projectId}" does not match your configured alias name "{graphqlField}" .
-        Make sure the value specified in the config.yaml file during DMS installation has been used
-        to create your project in the Arranger Admin UI.
-      </div>
-    );
-  }
-  return null;
-};
 
 const RepositoryPage = () => {
   const {
@@ -198,7 +99,7 @@ const RepositoryPage = () => {
   return (
     <PageLayout>
       {ConfigError ? (
-        <ConfigErrorDisplay>{ConfigError}</ConfigErrorDisplay>
+        <ErrorContainer title={'DMS Configuration Error'}>{ConfigError}</ErrorContainer>
       ) : (
         <Arranger
           api={arrangerFetcher}
