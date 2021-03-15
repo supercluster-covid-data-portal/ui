03dc0a7161422795b746e5b2e5648c6abe5812c2
Ann Catton <ann.catton@oicr.on.ca>
2021-03-15 16:50:24 -0400

    cleanup

--- components/theme/dimensions.ts
+++ components/theme/dimensions.ts
@@ -1,9 +1,3 @@
-// dimension constants
-export const FACET_MAX_WIDTH = 270;
-export const FACET_MIN_WIDTH = 250;
-export const FOOTER_HEIGHT = 47;
-export const NAVBAR_HEIGHT = 50;
-
 const dimensions = {
   navbar: {
     height: 50,
