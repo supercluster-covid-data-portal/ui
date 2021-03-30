f3b141147d176af0010c46dee98a0244404b42bd
Ann Catton <ann.catton@oicr.on.ca>
2021-03-30 12:56:03 -0400

    📄 Add copyright

--- components/pages/explorer/RepoTable.tsx
+++ components/pages/explorer/RepoTable.tsx
@@ -1,3 +1,24 @@
+/*
+ *
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ *  This program and the accompanying materials are made available under the terms of
+ *  the GNU Affero General Public License v3.0. You should have received a copy of the
+ *  GNU Affero General Public License along with this program.
+ *   If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ *  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 import { css } from '@emotion/core';
 import dynamic from 'next/dynamic';
 import urlJoin from 'url-join';
@@ -207,15 +228,19 @@ const getTableStyle = (theme: typeof defaultTheme) => css`
 `;
 
 const RepoTable = (props: PageContentProps) => {
-  const { NEXT_PUBLIC_ARRANGER_API, NEXT_PUBLIC_ARRANGER_PROJECT_ID, NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS } = getConfig();
-  const manifestColumns = NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS
-    .split(',').filter(field => field.trim()) // break it into arrays, and ensure there's no empty field names
-    .map(fieldName => fieldName.replace(/['"]+/g, '').trim());
+  const {
+    NEXT_PUBLIC_ARRANGER_API,
+    NEXT_PUBLIC_ARRANGER_PROJECT_ID,
+    NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS,
+  } = getConfig();
+  const manifestColumns = NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS.split(',')
+    .filter((field) => field.trim()) // break it into arrays, and ensure there's no empty field names
+    .map((fieldName) => fieldName.replace(/['"]+/g, '').trim());
 
-  const today = new Date().toISOString().slice(0,10).replace(/-/g,'');
+  const today = new Date().toISOString().slice(0, 10).replace(/-/g, '');
   const customExporters = [
-    {label: 'File Table', fileName: `data-explorer-table-export.${today}.tsv`}, // exports a TSV with what is displayed on the table (columns selected, etc.)
-    {label: 'File Manifest', fileName: `score-manifest.${today}.tsv` , columns: manifestColumns,}, // exports a TSV with the manifest columns
+    { label: 'File Table', fileName: `data-explorer-table-export.${today}.tsv` }, // exports a TSV with what is displayed on the table (columns selected, etc.)
+    { label: 'File Manifest', fileName: `score-manifest.${today}.tsv`, columns: manifestColumns }, // exports a TSV with the manifest columns
   ];
 
   return (
