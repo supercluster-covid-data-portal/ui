38cf3c6eaf025a5dddb4d23c0d94095cdcbeed45
Yelizar Alturmessov <65674408+yalturmes@users.noreply.github.com>
2021-07-21 10:31:44 -0400

    Update Jenkinsfile.groovy
    Set explicit HOME env var for docker client container.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,6 +21,8 @@ spec:
     env:
     - name: DOCKER_HOST
       value: tcp://localhost:2375
+    - name: HOME
+      value: /home/jenkins/agent
   - name: helm
     image: alpine/helm:3.1.0
     command:
