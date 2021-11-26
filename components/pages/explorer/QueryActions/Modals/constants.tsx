1d7ebff29f9289e75c19b4083ab92f2097a3cf6e
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2021-11-26 13:11:17 -0500

    [Query Storage] add edit queries functionality (#38)

--- components/pages/explorer/QueryActions/Modals/constants.tsx
+++ components/pages/explorer/QueryActions/Modals/constants.tsx
@@ -1,6 +1,20 @@
 import { QueryModalsData } from '../types';
 
 export const modalsData: QueryModalsData = {
+  editQuery: {
+    actionText: 'Update',
+    content: 'Edit the name of your saved query',
+    title: 'Edit Query',
+    type: 'input',
+  },
+  editQuery_success: {
+    content: 'Query updated succesfully!',
+    type: 'success',
+  },
+  editQuery_error: {
+    content: 'The query could not be updated.',
+    type: 'error',
+  },
   saveQuery: {
     actionText: 'Save',
     content: 'Save the current configuration of filters',
