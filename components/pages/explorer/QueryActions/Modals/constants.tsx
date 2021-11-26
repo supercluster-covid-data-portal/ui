cd91496f2b39eecbac5fc0f035afd9999cfd4327
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2021-11-26 13:13:53 -0500

    [Query Storage] add delete queries (#39)

--- components/pages/explorer/QueryActions/Modals/constants.tsx
+++ components/pages/explorer/QueryActions/Modals/constants.tsx
@@ -1,6 +1,25 @@
 import { QueryModalsData } from '../types';
 
 export const modalsData: QueryModalsData = {
+  deleteQuery: {
+    actionText: 'Delete',
+    content: (
+      <div>
+        <p>Are you sure you want to delete this query?</p>
+        <p>This action cannot be undone.</p>
+      </div>
+    ),
+    title: 'Delete Query',
+    type: 'confirmation',
+  },
+  deleteQuery_success: {
+    content: 'Query deleted succesfully!',
+    type: 'success',
+  },
+  deleteQuery_error: {
+    content: 'The query could not be deleted.',
+    type: 'error',
+  },
   editQuery: {
     actionText: 'Update',
     content: 'Edit the name of your saved query',
